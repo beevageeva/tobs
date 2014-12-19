475ef61339f9bfdee85578615aa070dcedafe37b
Beatrice Popescu Braileanu <bpb@yemen.cca.iac.es>
2014-12-19 07:35:54 +0000

    allstar

---
+++ allstar.py
@@ -0,0 +1,40 @@
+import numpy as np
+import sys
+from pyraf import iraf
+
+# Initialize IRAF with ccdred
+iraf.noao.digiphot.daophot(Stdout=1)
+
+def daofind(filelist):
+	iraf.datapars.setParam("exposure", "EXPTIME")
+	iraf.datapars.setParam("airmass", "AIRMASS")
+	iraf.datapars.setParam("filter", "INSFILTE")
+	iraf.datapars.setParam("ccdread", "RDNOISE")
+	iraf.datapars.setParam("gain", "GAIN")
+	
+	
+
+	with open(filelist) as file1:
+		for line in file1:
+			filename = line.strip()
+			y,z = np.loadtxt(filename + "-daoedit", usecols=[3,4], unpack=True)
+			sigma = np.mean(y) 
+			fwhm = np.mean(z)
+			iraf.datapars.setParam("fwhmpsf", "%s" % str(fwhm)  )
+			iraf.datapars.setParam("sigma", "%s" % str(sigma)  )
+
+			iraf.allstar.setParam("image","%s" % filename	)
+			iraf.allstar.setParam("photfile","%s.mag" % filename	)
+			iraf.allstar.setParam("psfimage","/scratch/M37New/object/V/Nov30098a.fits.psf.1.fits"	)
+			iraf.allstar.setParam("allstarfile","%s.als"%filename	)
+			iraf.allstar.setParam("rejfile","%s.arj"%filename	)
+			iraf.allstar.setParam("subimage","%s.sub" %filename	)
+
+
+			iraf.allstar(mode="h")
+			
+
+
+
+daofind(sys.argv[1])	
+
